6455eb1292d0a665edc03f008bbecc4d6f23d330
juanboro githubaction <29214674+juanboro@users.noreply.github.com>
2025-04-19 18:38:57 -0400

    Add tcp_server component

---
+++ esphome/components/tcp_server/tcp_server.h
@@ -0,0 +1,141 @@
+/*
+
+Partially based on: https://github.com/oxan/esphome-stream-server , Copyright (C) 2020-2022 Oxan van Leeuwen, License:
+GPL-V3 and: https://github.com/tube0013/esphome-stream-server-v2
+*/
+
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/helpers.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+
+#include <string>
+#include <vector>
+
+#ifdef USE_ESP_IDF
+#include "esphome/components/socket/socket.h"
+#else
+// https://registry.platformio.org/libraries/esphome/ESPAsyncTCP-esphome/examples/ClientServer/Server/Server.ino
+#ifdef ESP8266
+#include <ESPAsyncTCP.h>
+#else
+#include <AsyncTCP.h>
+#endif
+#endif
+
+namespace esphome {
+namespace tcp_server {
+
+class TCPServerTrigger : public Trigger<std::string, std::string> {};
+class TCPServerOnConnectTrigger : public Trigger<std::string> {};
+class TCPServerOnDisconnectTrigger : public Trigger<std::string> {};
+// mechanism for receiving messages directly to components...
+// yeah - this isn't quite the esphome way of doing this through python cv/cg, but I haven't quite figured that out...
+// there are hints in climate_ir ...
+using TCPServerReadCallBack = std::function<void(std::string, std::string)>;
+
+class TCPServerBaseComponent : public Component {
+ public:
+  TCPServerBaseComponent() {}
+
+  void setup();
+  virtual void loop() {}
+  void dump_config();
+  virtual void on_shutdown() {}
+
+  float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
+
+  virtual void write(const std::string &data) {}
+  virtual void write(const std::string &data, const std::string &client_id) {}
+  void register_onmessage_trigger(TCPServerTrigger *trig) { this->triggers_onmsg_.push_back(trig); }
+  void register_onconnect_trigger(TCPServerOnConnectTrigger *trig) { this->triggers_on_connect_.push_back(trig); }
+  void register_ondisconnect_trigger(TCPServerOnDisconnectTrigger *trig) {
+    this->triggers_on_disconnect_.push_back(trig);
+  }
+  void register_onread_callback(TCPServerReadCallBack readcb) { this->on_read_callbacks_.push_back(readcb); }
+
+  void set_port(uint16_t port) { this->port_ = port; }
+  virtual int get_client_count() { return 0; }
+  void set_count_sensor(binary_sensor::BinarySensor *sensor);
+
+ protected:
+  uint16_t port_{9000};
+
+  std::vector<TCPServerTrigger *> triggers_onmsg_;
+  std::vector<TCPServerOnConnectTrigger *> triggers_on_connect_;
+  std::vector<TCPServerOnDisconnectTrigger *> triggers_on_disconnect_;
+  std::vector<TCPServerReadCallBack> on_read_callbacks_;
+
+  binary_sensor::BinarySensor *count_sensor_ = nullptr;
+};
+
+#ifdef USE_ESP_IDF
+class TCPServerComponent : public TCPServerBaseComponent {
+ public:
+  TCPServerComponent() {}
+
+  void setup() override;
+  void loop() override;
+  void on_shutdown() override;
+
+  void write(const std::string &data);
+  void write(const std::string &data, const std::string &client_id);
+
+  int get_client_count() override { return this->clients_.size(); }
+
+ protected:
+  void accept();
+  void cleanup();
+  void read();
+
+  struct Client {
+    Client(std::unique_ptr<esphome::socket::Socket> socket, std::string identifier);
+    std::unique_ptr<esphome::socket::Socket> socket{nullptr};
+    std::string identifier{};
+    bool disconnected{false};
+  };
+
+  std::vector<Client> clients_{};
+  std::unique_ptr<esphome::socket::Socket> socket_{};
+};
+#else
+class TCPServerComponent : public TCPServerBaseComponent {
+ public:
+  TCPServerComponent() {}
+
+  void setup() override;
+  void loop() override;
+  void on_shutdown() override;
+
+  void write(const std::string &data);
+  void write(const std::string &data, const std::string &client_id);
+
+  int get_client_count() override { return this->clients_.size(); }
+
+ protected:
+  void cleanup();
+
+  struct Client {
+    Client(AsyncClient *client, std::string identifier);
+    AsyncClient *client{nullptr};
+    std::string identifier{};
+    bool disconnected{false};
+  };
+
+  std::vector<Client> clients_{};
+
+ private:
+  void handleNewClient(AsyncClient *client);
+  void handleError(AsyncClient *client, int8_t error);
+  void handleData(AsyncClient *client, void *data, size_t len);
+  void handleDisconnect(AsyncClient *client);
+  void handleTimeout(AsyncClient *client, uint32_t time);
+
+  AsyncServer *server_{};
+};
+#endif
+
+}  // namespace tcp_server
+}  // namespace esphome
