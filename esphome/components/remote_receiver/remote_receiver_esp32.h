e912649931a3f5a9fae2acc1a6983f396fe0b97b
juanboro githubaction <29214674+juanboro@users.noreply.github.com>
2025-06-22 15:39:52 -0400

    additional merge updates

--- esphome/components/remote_receiver/remote_receiver_esp32.h
+++ esphome/components/remote_receiver/remote_receiver_esp32.h
@@ -4,17 +4,12 @@
 
 #include "esphome/components/remote_base/remote_base.h"
 #include "esphome/core/component.h"
-
-#include <cinttypes>
-
-#if ESP_IDF_VERSION_MAJOR >= 5
 #include <driver/rmt_rx.h>
-#endif
+#include <cinttypes>
 
 namespace esphome {
 namespace remote_receiver_esp32 {
 
-#if ESP_IDF_VERSION_MAJOR >= 5
 struct RemoteReceiverComponentStore {
   /// Stores RMT symbols and rx done event data
   volatile uint8_t *buffer{nullptr};
@@ -29,7 +24,6 @@ struct RemoteReceiverComponentStore {
   esp_err_t error{ESP_OK};
   rmt_receive_config_t config;
 };
-#endif
 
 class RemoteReceiverComponent : public remote_base::RemoteReceiverBase,
                                 public Component,
@@ -37,48 +31,28 @@ class RemoteReceiverComponent : public remote_base::RemoteReceiverBase,
 
 {
  public:
-#if ESP_IDF_VERSION_MAJOR < 5
-  RemoteReceiverComponent(InternalGPIOPin *pin, uint8_t mem_block_num = 1)
-      : RemoteReceiverBase(pin), remote_base::RemoteRMTChannel(mem_block_num) {}
-
-  RemoteReceiverComponent(InternalGPIOPin *pin, rmt_channel_t channel, uint8_t mem_block_num = 1)
-      : RemoteReceiverBase(pin), remote_base::RemoteRMTChannel(channel, mem_block_num) {}
-#else
   RemoteReceiverComponent(InternalGPIOPin *pin) : RemoteReceiverBase(pin) {}
-#endif
   void setup() override;
   void dump_config() override;
   void loop() override;
-#if ESP_IDF_VERSION_MAJOR >= 5
   float get_setup_priority() const override { return setup_priority::DATA; }
   void set_filter_symbols(uint32_t filter_symbols) { this->filter_symbols_ = filter_symbols; }
   void set_receive_symbols(uint32_t receive_symbols) { this->receive_symbols_ = receive_symbols; }
   void set_with_dma(bool with_dma) { this->with_dma_ = with_dma; }
-#else
-  float get_setup_priority() const override { return setup_priority::DATA - 2; }
-#endif
   void set_buffer_size(uint32_t buffer_size) { this->buffer_size_ = buffer_size; }
   void set_filter_us(uint32_t filter_us) { this->filter_us_ = filter_us; }
   void set_idle_us(uint32_t idle_us) { this->idle_us_ = idle_us; }
 
  protected:
-#if ESP_IDF_VERSION_MAJOR >= 5
   void decode_rmt_(rmt_symbol_word_t *item, size_t item_count);
   rmt_channel_handle_t channel_{NULL};
   uint32_t filter_symbols_{0};
   uint32_t receive_symbols_{0};
   bool with_dma_{false};
-#else
-  void decode_rmt_(rmt_item32_t *item, size_t item_count);
-  RingbufHandle_t ringbuf_;
-#endif
   esp_err_t error_code_{ESP_OK};
   std::string error_string_{""};
-
-#if ESP_IDF_VERSION_MAJOR >= 5
   RemoteReceiverComponentStore store_;
   HighFrequencyLoopRequester high_freq_;
-#endif
 
   uint32_t buffer_size_{};
   uint32_t filter_us_{10};
